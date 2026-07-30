326c30c4d67a47a5ae8ed7cd58bb768e034e886b
dasun@mylinex.com <dasun@mylinex.com>
2026-07-30 12:56:29 +0530

    fix: password history service provider issue

--- src/PasswordHistoryServiceProvider.php
+++ src/PasswordHistoryServiceProvider.php
@@ -16,25 +16,29 @@ class PasswordHistoryServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
-        $this->loadTranslationsFrom(__DIR__ . '/../lang', 'password-history');
-
-        $this->publishes([
-            __DIR__ . '/../config/password-history.php' => config_path('password-history.php'),
-        ], 'password-history-config');
-
-        $this->publishesMigrations([
-            __DIR__ . '/../database/migrations' => database_path('migrations'),
-        ], 'password-history-migrations');
-
-        $this->publishes([
-            __DIR__ . '/../lang' => $this->app->langPath('vendor/password-history'),
-        ], 'password-history-lang');
-
         if ($this->app->runningInConsole()) {
+            $this->loadTranslationsFrom(__DIR__ . '/../lang', 'password-history');
+
+            $this->publishes([
+                __DIR__ . '/../config/password-history.php' => config_path('password-history.php'),
+            ], 'password-history-config');
+
+            $this->publishesMigrations([
+                __DIR__ . '/../database/migrations' => database_path('migrations'),
+            ], 'password-history-migrations');
+
+            $this->publishes([
+                __DIR__ . '/../lang' => $this->app->langPath('vendor/password-history'),
+            ], 'password-history-lang');
             $this->commands([
                 \DevDasun\PasswordHistory\Console\Commands\PrunePasswordHistory::class,
             ]);
+        }
+
+        $publishedMigration = glob(database_path('migrations/*_create_password_history_table.php'));
+        if (empty($publishedMigration)) {
+            // Automatically load migrations only if they have not been published
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
         }
     }
 }
